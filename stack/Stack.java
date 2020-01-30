acffcf9881a10754ade9d683821f0dd318c54517
alecavi <acavicchioli@dundee.ac.uk>
2020-01-30 16:53:55 +0000

    created basic stack

--- stack/Stack.java
+++ stack/Stack.java
@@ -7,10 +7,38 @@ import linkedList.List;
 
 /**
  * A stack data structure, backed by a linked list capable of storing id-mark pairs
+ * 
  * @author Alessandro Cavicchioli
  * @version 1.0
  */
 public class Stack
 {
-	List stack;
+	private List list;
+	
+	/**
+	 * Returns whether the stack has 0 elements
+	 * @return {@code true} if the stack is empty, {@code false} otherwise
+	 */
+	public boolean isEmpty()
+	{
+		return list.size() == 0;
+	}
+	
+	/**
+	 * Pushes the specified value on the stack
+	 * @param value the value to push
+	 */
+	public void push(int value)
+	{
+		list.addToList(value);
+	}
+	
+	/**
+	 * Pops the top value from the stack
+	 * @return the value that was removed from the top of the stack
+	 */
+	public int pop()
+	{
+		return list.removeFirst();
+	}
 }
