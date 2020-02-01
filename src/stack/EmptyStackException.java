323910131ccf07aa5a62859f509907e13678c0af
alecavi <acavicchioli@dundee.ac.uk>
2020-02-01 19:06:37 +0000

    Added serialVersionUID

--- src/stack/EmptyStackException.java
+++ src/stack/EmptyStackException.java
@@ -2,8 +2,9 @@ package stack;
 
 public class EmptyStackException extends RuntimeException 
 {
+	private static final long serialVersionUID = 1L;
 
-    /** Constructs a new exception with {@code null} as its
+	/** Constructs a new exception with {@code null} as its
      * detail message.  The cause is not initialized, and may subsequently be
      * initialized by a call to {@link #initCause}.
      */
