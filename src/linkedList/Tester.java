2beada004e8e54c4d999d2346b6242521679776c
alecavi <acavicchioli@dundee.ac.uk>
2020-02-01 19:33:49 +0000

    Fixed author name

--- src/linkedList/Tester.java
+++ src/linkedList/Tester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * An automated testing class for the linked list.
  * 
- * @author acavicchioli
+ * @author Alessandro Cavicchioli
  * @version 1.0
  */
 public class Tester
