21e6392e145c10c5aeb0920e6d9362d8df316ff4
alecavi <acavicchioli@dundee.ac.uk>
2020-02-02 12:42:32 +0000

    Added main menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,24 +10,81 @@ public class Main
 	
 	public static void main(String[] args) 
 	{
-		RpnCalculator calculator = new RpnCalculator();
+		Tester tester = new Tester();
+		Main main = new Main();
 		
-		System.out.println("Input a postfix expression");
-		String expression = scanner.nextLine();
+		main.printInstructions();
+		boolean exit = false;
+		while(!exit)
+		{
+			String input = scanner.nextLine();
+			switch(input.toLowerCase())
+			{
+			case "test":
+				tester.menu();
+				main.printInstructions();
+				break;
+			case "help":
+				main.printInstructions();
+				break;
+			case "exit":
+				exit = true;
+				break;
+			default:
+				main.solvePostfix(input);
+				break;	
+			}
+		}
+		scanner.close();
+	}
+	
+	/**
+	 * Prints the instructions to use the main menu.
+	 */
+	private void printInstructions()
+	{
+		System.out.println("Input a postfix expression to see the solution and an equivalent"
+				+ " infix expression, \"test\" to enter testing mode, \"help\" to see this message,"
+				+ " or \"exit\" to exit");
+	}
+	
+	/**
+	 * Solves the expression passed as argument and prints the result and an equivalent infix
+	 * expression, or an error message if the expression is invalid
+	 * @param expression
+	 */
+	private void solvePostfix(String expression)
+	{
+		RpnCalculator calculator = new RpnCalculator();
 		
 		if(calculator.validate(expression))
 		{
-			int result = calculator.calculate(expression);
-			String infix = calculator.infixEquivalent(expression);
-			
-			System.out.println("result = " + result);
-			System.out.println("Equivalent infix expression = " + infix);
-			
+			System.out.println("Result = " + calculator.calculate(expression));
+			System.out.println("Equivalent infix expression = " + calculator.infixEquivalent(expression));
 		}
 		else
 		{
 			System.out.println("Invalid expression");
 		}
-		scanner.close();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
